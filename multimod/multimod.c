556e1c36d54c7e647a9d5553d2487b6d30bb8e23
tracer-nju <tracer@nju.edu.cn>
2020-10-03 17:27:58 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 2ef259c4c72948548bb994fd12b2c367            Boot ID: 9139b9c0b29f469593a0c346f840f630   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  17:27:58 up  5:48,  1 user,  load average: 0.43, 0.46, 0.37

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,99 @@
 #include <stdint.h>
 
-uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+
+static inline int bit_of(uint64_t x, int i) 
+{
+    return (x >> i) & 1;
+}//取位运算
+
+static inline  void right(short * a,int x)
+{
+    for(int i = 0;i < 63;i++)
+    {
+        a[i]=a[i+1];
+    }
+    a[63]=0;
+}
+
+static inline void sub(short *a,short *b)
+{
+    short cin=0;
+    for(int i= 0;i < 128;i++)
+    {
+        if(a[i] >= b[i]+cin)
+        {
+            a[i]=a[i]-b[i]-cin;
+            cin=0;
+        }
+        else{
+            a[i]=a[i]+2-b[i]-cin;
+            cin=1;
+        }
+    }
+}
+
+static inline short com(short *a,short *b)
+{
+    for(int i = 127;i>=0;i--)
+    {
+        if(a[i] > b[i])
+        {
+            return 1;
+        }
+        else if(a[i] < b[i]){
+            return 0;
+        }
+        else continue;
+    }
+    return 1;
+}
+
+uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) 
+{
+    short int x[64]={0},y[64]={0},cin=0,ans[128]={0},mod[128];
+    int num=0;
+    uint64_t ret=0;
+    for(int i = 0;i < 64;i++)
+    {
+        x[i]=bit_of(a,i);
+        y[i]=bit_of(b,i);
+    }
+    for(int i = 0;i < 128;i++)
+        {
+            for(int j = i > 63? i-63:0;j < (i > 63? 64: i+1); j++)
+            {
+                ans[i]=ans[i]+(x[j]&y[i-j]);
+            }
+            ans[i]=ans[i]+cin;
+            cin=ans[i]>>1;
+            ans[i]=ans[i]&1;
+        }
+    
+    for(int i= 63;i >= 0;i--)
+    {
+        if(bit_of(m,i)==1)
+        {
+            num=i;
+            break;
+        }
+    }
+
+    for(int i = num;i >= 0;i--)
+    {
+        mod[127-num+i]=mod[i];
+    }
+    while(num < 127)
+    {
+        if(com(ans,mod))
+        {
+            sub(ans,mod);
+        }
+        num++;
+        right(mod,1);
+    }
+    for(int i = 63;i >= 0;i--)
+    {
+        ret=(ret<<1)+ans[i];
+    }
+    return ret;
 }
