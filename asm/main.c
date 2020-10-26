98b0a6f733ec4c5138c9ca0558bfc1690ae174c2
tracer-nju <tracer@nju.edu.cn>
2020-10-26 21:13:01 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 2ef259c4c72948548bb994fd12b2c367            Boot ID: df6fac9d6b324798a8e700b484af71ba   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-11-amd64       Architecture: x86-64  21:13:01 up  3:16,  1 user,  load average: 0.38, 0.47, 0.55

--- asm/main.c
+++ asm/main.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 
 int main() {
-  //asm_jmp_buf buf;
+  asm_jmp_buf buf;
     assert(asm_add(1234, 5678) == 6912);
 //  int r = asm_setjmp(buf);
 //  if (r == 0) {
 //    assert(asm_add(1234, 5678) == 6912);
-////    assert(asm_popcnt(0x0123456789abcdefULL) == 32);
-////    // TODO: add more tests here.
-////    asm_longjmp(buf, 123);
+//    assert(asm_popcnt(0x0123456789abcdefULL) == 32);
+//    // TODO: add more tests here.
+//    asm_longjmp(buf, 123);
 //  } else {
 //    assert(r == 123);
 //    printf("PASSED.\n");
