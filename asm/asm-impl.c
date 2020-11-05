26d56e9c13b879d2733f02ba2df80a4968d7d427
tracer-nju <tracer@nju.edu.cn>
2020-11-05 21:05:30 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 2ef259c4c72948548bb994fd12b2c367            Boot ID: f80b6252dc5543638dafd1a21e8b76d3   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-11-amd64       Architecture: x86-64  21:05:30 up 45 min,  1 user,  load average: 0.86, 1.15, 0.99

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -18,7 +18,7 @@ int asm_popcnt(uint64_t x) {
     }
   asm(
       ".loop:\n"
-          "mov %2,%1;"
+          "movl %2,%1;"
           "shr %3,%1;"
           "and $0x1,%1;"
           "add %1,%0;"
