2b7cc85d5e0ed11f9b86de0dc13213f1b00ea2ef
tracer-nju <tracer@nju.edu.cn>
2020-11-09 16:43:15 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 2ef259c4c72948548bb994fd12b2c367            Boot ID: 3b329debf7ec460998b76ec0de01cddb   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-11-amd64       Architecture: x86-64  16:43:15 up 16 min,  1 user,  load average: 0.69, 0.78, 0.50

--- asm/main.c
+++ asm/main.c
@@ -9,7 +9,7 @@ int main() {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_add(1,2) == 3);
     assert(asm_add(12313,123123) == 12313+123123);
-    assert(asm_popcnt(0xff)==8);
+    assert(asm_popcnt(0x1)==1);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     // TODO: add more tests here.
     asm_longjmp(buf, 123);
