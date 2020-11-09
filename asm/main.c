4196c6bff5c7e858aafffea4ed751bba7c37291c
tracer-nju <tracer@nju.edu.cn>
2020-11-09 20:40:44 +0800

    Static hostname: debian          Icon name: computer-laptop            Chassis: laptop         Machine ID: 2ef259c4c72948548bb994fd12b2c367            Boot ID: 6263a216c50a46449d5b5b66b7cbf36a   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-11-amd64       Architecture: x86-64  20:40:44 up 16 min,  1 user,  load average: 1.12, 1.06, 0.68

--- asm/main.c
+++ asm/main.c
@@ -4,13 +4,17 @@
 
 int main() {
   asm_jmp_buf buf;
+  char* src[10]={'a','a','a'};
+  char* dst[10]={0};
   int r = asm_setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
-    assert(asm_add(1,2) == 3);
-    assert(asm_add(12313,123123) == 12313+123123);
-    assert(asm_popcnt(0x1)==1);
-    assert(asm_popcnt(0x0123456789abcdefULL) == 32);
+    //assert(asm_add(1,2) == 3);
+    //assert(asm_add(12313,123123) == 12313+123123);
+    //assert(asm_popcnt(0x1)==1);
+    //assert(asm_popcnt(0x0123456789abcdefULL) == 32);
+    asm_memcpy(dst,src,3);
+    assert(dst[0] == src[0]);
     // TODO: add more tests here.
     asm_longjmp(buf, 123);
   } else {
